a3253933d6d46dfa7a8c0952a781f22860927192
Philippe Hiroux <12002903@student.pxl.be>
2024-07-15 12:02:56 +0200

    capsense function for button 0, button 1 and slider

--- Clock/capsense.c
+++ Clock/capsense.c
@@ -12,7 +12,9 @@ bool scanComplete = false;
 void initCapSense(void);										//Declare the function initCapSense
 static void capsense_isr(void);									//Declare the function capsense_isr
 void endScanCallback(cy_stc_active_scan_sns_t * ptrActiveScan);	//Declare the function endScanCallback
-void processTouch(void);										//Declare the fucntion processTouch
+void capsenseSlider(void);										//Declare the fucntion processTouch
+void capsenseButtonLeft(void);
+void capsenseButtonRight(void);
 
 int i = 0;	//Variable to save the index for the flash memory
 
@@ -28,7 +30,9 @@ void capsense(void)
     	{
     		Cy_CapSense_ProcessAllWidgets(&cy_capsense_context);	//Process scan data
     		Cy_CapSense_RunTuner(&cy_capsense_context);				//Update tuner
-    		processTouch();											//Update LED according to any touches
+    		capsenseSlider();											//Update LED according to any touches
+    		capsenseButtonLeft();
+    		capsenseButtonRight();
 			Cy_CapSense_ScanAllWidgets(&cy_capsense_context);		//Start next scan
 			scanComplete = false;									//Make scanComplete equal to false
     	}
@@ -51,52 +55,59 @@ void initCapSense(void) //Initializes the CSD HW block for touch sensing
 	Cy_CapSense_RegisterCallback(CY_CAPSENSE_END_OF_SCAN_E, endScanCallback, &cy_capsense_context);	//Initialize end of scan callback
 }
 
-void processTouch(void) 						//Check if CapSense buttons were pressed and update the LED state accordingly
+void capsenseButtonLeft(void)
 {
 	uint32_t button0_status;					//Vars to hold widget statuses
-	uint32_t button1_status;					//Vars to hold widget statuses
-	cy_stc_capsense_touch_t *slider_touch_info;	//Vars to hold widget statuses
-	uint16_t slider_pos;						//Vars to hold widget statuses
-	uint8_t slider_touch_status;				//Vars to hold widget statuses
 	static uint32_t button0_status_prev;		//Vars to hold previous widget statuses
-	static uint32_t button1_status_prev;		//Vars to hold previous widget statuses
-	static uint16_t slider_pos_prev;			//Vars to hold previous widget statuses
-
 	button0_status = Cy_CapSense_IsSensorActive(CY_CAPSENSE_BUTTON0_WDGT_ID, CY_CAPSENSE_BUTTON0_SNS0_ID, &cy_capsense_context);	//Get button0 status
-	button1_status = Cy_CapSense_IsSensorActive(CY_CAPSENSE_BUTTON1_WDGT_ID, CY_CAPSENSE_BUTTON1_SNS0_ID, &cy_capsense_context);	//Get button1 status
 
-	slider_touch_info = Cy_CapSense_GetTouchInfo(CY_CAPSENSE_LINEARSLIDER0_WDGT_ID, &cy_capsense_context);	//Get slider status
-	slider_touch_status = slider_touch_info->numPosition;													//Get slider status
-	slider_pos = slider_touch_info->ptrPosition->x;															//Get slider status
-
-	if((button0_status != 0U) && (button0_status != button0_status_prev)){	//If button 0 of capsense has been pressed
-		printf("One meter forward\r\n");									//Print out the text
-		i++;																//Do i + 1
-		//robot_flash_read();
-	}
-
-	if((button1_status != 0U) && (button1_status != button1_status_prev)){	//If button 1 of capsense has been pressed
-		printf("One meter backward\r\n");									//Print out the text
-		i++;																//Do i + 1
-		//robot_flash_read();
-	}
-
-	// Print value when slider is pressed
-	if(slider_touch_status != 0U && slider_pos != slider_pos_prev) {
-		float value = (1.0 - ((float)slider_pos / (float)cy_capsense_context.ptrWdConfig[0].xResolution)) * 100;
-		printf("Slider pressed, value: %.2f\n", value);
-		slider_pos_prev = slider_pos; // Update previous position
-	}
-
-	// Print message when slider is released
-	if(slider_touch_status == 0U && slider_pos_prev != 0) {
-		printf("Slider released\n");
-		slider_pos_prev = 0; // Reset previous position
+	if((button0_status != 0U) && (button0_status != button0_status_prev))
+	{
+		printf("left button\r\n");									//Print out the text
 	}
 
 	button0_status_prev = button0_status;	//Update previous touch status
+}
+
+void capsenseButtonRight(void)
+{
+	uint32_t button1_status;					//Vars to hold widget statuses
+	static uint32_t button1_status_prev;		//Vars to hold previous widget statuses
+	button1_status = Cy_CapSense_IsSensorActive(CY_CAPSENSE_BUTTON1_WDGT_ID, CY_CAPSENSE_BUTTON1_SNS0_ID, &cy_capsense_context);	//Get button1 status
+
+	if((button1_status != 0U) && (button1_status != button1_status_prev))
+	{
+		printf("Right button\r\n");									//Print out the text
+	}
+
 	button1_status_prev = button1_status;	//Update previous touch status
-	slider_pos_prev = slider_pos;			//Update previous touch status
+}
+
+void capsenseSlider(void) 						//Check if CapSense buttons were pressed and update the LED state accordingly
+{
+	cy_stc_capsense_touch_t *slider_touch_info;	// Vars to hold widget statuses
+	uint16_t slider_pos = 0;					// Vars to hold widget statuses
+	uint8_t slider_touch_status;				// Vars to hold widget statuses
+	static uint8_t was_touched = 0;				// Vars to track if slider was previously touched
+
+	// Get slider status
+	slider_touch_info = Cy_CapSense_GetTouchInfo(CY_CAPSENSE_LINEARSLIDER0_WDGT_ID, &cy_capsense_context);
+	slider_touch_status = slider_touch_info->numPosition;
+	slider_pos = slider_touch_info->ptrPosition->x;
+
+	if((slider_touch_status != 0U) && (was_touched == 0U))
+	{
+		float value = (1.0 - ((float)slider_pos / (float)cy_capsense_context.ptrWdConfig[CY_CAPSENSE_LINEARSLIDER0_WDGT_ID].xResolution)) * 100; //Calculate de value of the slider
+		printf("Value aanraken: %.0f\r\n", value);
+		was_touched = 1;  // Update touch status
+	}
+
+	if (slider_touch_status == 0U && was_touched == 1U)
+	{
+		float value = (1.0 - ((float)slider_pos / (float)cy_capsense_context.ptrWdConfig[CY_CAPSENSE_LINEARSLIDER0_WDGT_ID].xResolution)) * 100; //Calculate de value of the slider
+		printf("Value loslaten: %.0f\r\n", value);
+		was_touched = 0;  // Update touch status
+	}
 }
 
 static void capsense_isr(void){Cy_CapSense_InterruptHandler(CYBSP_CSD_HW, &cy_capsense_context);}	//Interrupt Service Routines and Callbacks

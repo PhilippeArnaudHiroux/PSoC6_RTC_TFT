8daf60a1a032d1cd1e0b1b9d0091e7730c314e31
Philippe Hiroux <12002903@student.pxl.be>
2024-07-18 15:29:15 +0200

    Display time and date on tft shield

--- Clock/main.c
+++ Clock/main.c
@@ -5,16 +5,15 @@
 #include "GUI.h"
 
 #define TM_YEAR_BASE (1900u)
-#define RTC_CALLBACK_ARG (NULL)
-#define RTC_INTERRUPT_PRIORITY (1u)
 
 cyhal_rtc_t 	my_rtc;
 cyhal_timer_t 	timer_obj;
-char        	buffer[80];
+
+char buffer[80];
 
 const int mday = 3, month = 3, year = 2020; // Day of month, month and year
 const int hours = 8, minutes = 10, seconds = 0; // Hours, minutes and seconds
-const int wday = 3; // Days from Sunday. Sunday is 0, Monday is 1 and so on.
+const int wday = 4; // Days from Sunday. Sunday is 0, Monday is 1 and so on.
 const int dst  = 0; // Daylight Savings. 0 - Disabled, 1 - Enabled
 
 // Setting the tm structure as 08 HRS:10 MIN:10 SEC ; 3rd March 2020; Wednesday ; DST off
@@ -29,7 +28,9 @@ struct tm new_date_time =
 	.tm_wday  = wday,
 	.tm_isdst = dst
 };
-struct tm current_date_time = {0};
+struct tm current_date_timee = {0};
+
+void printfTFT(cyhal_rtc_t *rtc_ptr);
 
 const cyhal_timer_cfg_t timer_cfg =
 {
@@ -41,9 +42,14 @@ const cyhal_timer_cfg_t timer_cfg =
 	.value         = 0                   // Initial value of counter
 };
 
-//void snippet_cyhal_timer_event_interrupt();
-static void isr_timer(void* callback_arg, cyhal_timer_event_t event);
+static void isr_timer(void* callback_arg, cyhal_timer_event_t event)
+{
+	cyhal_rtc_read(&my_rtc, &current_date_timee);
+	strftime(buffer, sizeof(buffer), "%c", &current_date_timee);
+	printf("%s - TIMER\r\n", buffer);
 
+	printfTFT(&my_rtc);
+}
 
 int main(void)
 {
@@ -71,48 +77,10 @@ int main(void)
 	cyhal_timer_enable_event(&timer_obj, CYHAL_TIMER_IRQ_TERMINAL_COUNT, 3, true);
 	cyhal_timer_start(&timer_obj);
 
-	GUI_SetFont(&GUI_FontD80);
-
-	cyhal_rtc_read(&my_rtc, &current_date_time);
-	strftime(buffer, sizeof(buffer), "%H:%M", &current_date_time);
-	GUI_DispStringAt(buffer, 0, 0);
-
-	printf("DONEE\r\n");
-
-	//snippet_cyhal_timer_event_interrupt();
+	printfTFT(&my_rtc);
 
     for(;;)
     {
     	cyhal_syspm_sleep();
     }
 }
-
-static void isr_timer(void* callback_arg, cyhal_timer_event_t event)
-{
-	cyhal_rtc_read(&my_rtc, &current_date_time);
-	strftime(buffer, sizeof(buffer), "%c", &current_date_time);
-	printf("%s - TIMER\r\n", buffer);
-	strftime(buffer, sizeof(buffer), "%H:%M", &current_date_time);
-	GUI_DispStringAt(buffer, 0, 0);
-}
-/*
-void snippet_cyhal_timer_event_interrupt()
-{
-    const cyhal_timer_cfg_t timer_cfg =
-    {
-        .compare_value = 0,                  // Timer compare value, not used
-        .period        = 600000,             // Defines the timer period for 15 seconds
-        .direction     = CYHAL_TIMER_DIR_UP, // Timer counts up
-        .is_compare    = false,              // Don't use compare mode
-        .is_continuous = true,               // Run the timer indefinitely
-        .value         = 0                   // Initial value of counter
-    };
-
-    cyhal_timer_init(&timer_obj, NC, NULL);
-    cyhal_timer_configure(&timer_obj, &timer_cfg);
-    cyhal_timer_set_frequency(&timer_obj, 10000);
-	cyhal_timer_register_callback(&timer_obj, isr_timer, NULL);
-	cyhal_timer_enable_event(&timer_obj, CYHAL_TIMER_IRQ_TERMINAL_COUNT, 3, true);
-	cyhal_timer_start(&timer_obj);
-}
-*/
---
+++ Clock/tftDisplay.c
@@ -0,0 +1,56 @@
+#include "cyhal.h"
+#include "cy8ckit_028_tft.h"
+#include "GUI.h"
+
+struct tm current_date_time = {0};
+//x = 320 en y = 240
+
+#define	HOUR_MINUTE_X  	22
+#define	HOUR_MINUTE_Y  	0
+#define	SECOND_X		174
+#define SECOND_Y  		100
+#define WEEKDAY_X  		22
+#define WEEKDAY_Y		100
+#define DAY_MONTH_X		22
+#define DAY_MONTH_Y		130
+#define YEAR_X			22
+#define YEAR_Y			160
+
+char hourMinuteBuf[8];
+char secondBuf[4];
+char weekdayBuf[10];
+char dayMonthBuf[10];
+char yearBuf[8];
+
+void printfTFT(cyhal_rtc_t *rtc_ptr)
+{
+	cyhal_rtc_read(rtc_ptr, &current_date_time);
+
+	//Create hour and minute
+	GUI_SetFont(&GUI_FontD80);
+	strftime(hourMinuteBuf, sizeof(hourMinuteBuf), "%H:%M", &current_date_time);
+	GUI_DispStringAt(hourMinuteBuf, HOUR_MINUTE_X, HOUR_MINUTE_Y);
+
+	//Create second
+	GUI_SetFont(&GUI_FontD80);
+	strftime(secondBuf, sizeof(secondBuf), "%S", &current_date_time);
+	GUI_DispStringAt(secondBuf, SECOND_X, SECOND_Y);
+
+	//Create weekday
+	GUI_SetFont(&GUI_Font32B_ASCII);
+	strftime(weekdayBuf, sizeof(weekdayBuf), "%A", &current_date_time);
+	GUI_DispStringAt(weekdayBuf, WEEKDAY_X, WEEKDAY_Y);
+
+	//Create day and month
+	GUI_SetFont(&GUI_Font32B_ASCII);
+	strftime(dayMonthBuf, sizeof(dayMonthBuf), "%d %b", &current_date_time);
+	GUI_DispStringAt(dayMonthBuf, DAY_MONTH_X, DAY_MONTH_Y);
+
+	//Create year
+	GUI_SetFont(&GUI_FontD32);
+	strftime(yearBuf, sizeof(yearBuf), "%Y", &current_date_time);
+	GUI_DispStringAt(yearBuf, YEAR_X, YEAR_Y);
+}
+
+
+

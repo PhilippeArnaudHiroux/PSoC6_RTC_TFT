108e33311f09ae65debdead0a0d89ffe04deb89c
Philippe Hiroux <12002903@student.pxl.be>
2024-07-18 10:20:01 +0200

    Incude tft

--- Clock/main.c
+++ Clock/main.c
@@ -1,20 +1,28 @@
 #include "cyhal.h"
 #include "cybsp.h"
 #include "cy_retarget_io.h"
-
-void capsense(void);
+#include "cy8ckit_028_tft.h"
+#include "GUI.h"
 
 int main(void)
 {
     cybsp_init();
     __enable_irq();
 
-    cy_retarget_io_init(CYBSP_DEBUG_UART_TX, CYBSP_DEBUG_UART_RX, CY_RETARGET_IO_BAUDRATE); //Init UART
-	printf("\x1b[2J\x1b[;H");
+	cy_retarget_io_init(CYBSP_DEBUG_UART_TX, CYBSP_DEBUG_UART_RX, CY_RETARGET_IO_BAUDRATE);
+    cy8ckit_028_tft_init (NULL, NULL, NULL, NULL);
+    
+    /* Initialize the emwin library */
+	GUI_Init();
+	GUI_SetFont(&GUI_FontD80);
 
-	capsense();
 
-    for (;;)
+	//print headers
+	GUI_DispString("12:45");
+	//GUI_DispStringAt("Accel: X:       Y:       Z:", 0, 10);
+	//GUI_DispStringAt("Gyro:  X:       Y:       Z:", 0, 20);
+
+    for(;;)
     {
     }
 }
